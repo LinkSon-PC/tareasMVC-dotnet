bf716b89fcf6c68afd14de42372bfd22e7bdb34a
Anthony Son <tonyfernandosonmux@gmail.com>
2024-05-04 21:43:55 -0600

    first commit

---
+++ TareasMVC/Migrations/20240411060823_TituloCambioNombre.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class TituloCambioNombre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "String",
+                table: "Tareas",
+                newName: "Titulo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Titulo",
+                table: "Tareas",
+                newName: "String");
+        }
+    }
+}
---
+++ TareasMVC/Migrations/20240413012545_AdminRol.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            IF NOT EXISTS (SELECT Id FROM AspNetRoles WHERE Id = '1d6f98ee-fb6b-4c07-8266-7db02b7297a8')
+            BEGIN
+	            INSERT INTO AspNetRoles (Id, [Name], [NormalizedName])
+	            VALUES ('1d6f98ee-fb6b-4c07-8266-7db02b7297a8','admin','ADMIN')
+            END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE AspNetRoles WHERE Id='1d6f98ee-fb6b-4c07-8266-7db02b7297a8'");
+        }
+    }
+}
